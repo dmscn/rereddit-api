44333822273e42c14304bab318d54ccce879f0c0
Leonardo Damasceno <leo.dsc@live.com>
2018-10-26 17:15:37 -0300

    Created .editorconfig file

--- src/bin/www.js
+++
@@ -1,8 +0,0 @@
-import { Bristol } from 'bristol';
-import palin from 'palin';
-
-const log = new Bristol();
-
-log.addTarget('console').withFormatter(palin);
-
-log.info("We're up and running");
---
+++ src/lib/logger.js
@@ -0,0 +1,6 @@
+import { Bristol } from 'bristol';
+import palin from 'palin';
+
+const logger = new Bristol()
+logger.addTarget("console").withFormatter(palin);
+export default logger;
