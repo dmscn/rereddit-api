96d36cc10766f2bfd53dd1e2e320ca5457fe1c23
Leonardo Damasceno <leo.dsc@live.com>
2018-11-04 23:42:34 -0200

    Created test bones for contact-service

---
+++ src/services/__tests__/contact-service.test.js
@@ -0,0 +1,21 @@
+describe('ContactService', () => {
+	describe('getAll', () => {
+		it('should test', () => {});
+	});
+
+	describe('getSingle', () => {
+		it('should test', () => {});
+	});
+
+	describe('add', () => {
+		it('should test', () => {});
+	});
+
+	describe('update', () => {
+		it('should test', () => {});
+	});
+
+	describe('remove', () => {
+		it('should test', () => {});
+	});
+});
