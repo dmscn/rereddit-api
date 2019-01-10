b762f4c9b6582414e26d49761c0239200aa8331e
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-01-09 22:31:49 -0200

    Added parentPost to Post Schema

--- src/models/post-model.js
+++ src/models/post-model.js
@@ -3,26 +3,31 @@ import mongoose, { Schema } from 'mongoose';
 mongoose.Promise = global.Promise;
 
 export const postSchema = Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	content: {
-		type: String,
-		required: true
-	},
-	author: {
-		type: { type: Schema.Types.ObjectId, ref: 'User' },
-		required: false
-	},
-	date: {
-		type: Date,
-		default: Date.now
-	},
-	replies: {
-		type: [{ type: Schema.Types.ObjectId, ref: 'Reply' }],
-		required: false
-	}
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: { type: Schema.Types.ObjectId, ref: 'User' },
+    required: false
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  parentPost: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    required: false,
+    default: null
+  },
+  replies: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    required: false
+  }
 });
 
 export default mongoose.model('Post', postSchema);
