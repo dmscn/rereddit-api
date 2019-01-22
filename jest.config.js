69b9e27bc7cf71a74e57193fe0a8613afbb3475d
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-01-22 12:19:50 -0200

    Changed jes.config.js to support TypeScript

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,28 @@
 const { defaults } = require('jest-config');
 
 module.exports = {
-	testMatch: ['**/__tests__/**/*.test.js'],
-	modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules/'],
-	transform: {
-		'^.+\\.jsx?$': 'babel-jest'
-	},
-	testEnvironment: 'node',
-	collectCoverageFrom: ['<rootDir>/src/**/*.js'],
-	coveragePathIgnorePatterns: [
-		'<rootDir>/node_modules/',
-		'<rootDir>/__tests__/',
-		'<rootDir>/src/lib/env',
-		'<rootDir>/src/bin',
-		'<rootDir>/src/models',
-		'<rootDir>/dist/'
-	],
-	moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'jsx']
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/__tests__/',
+    '<rootDir>/src/lib/env',
+    '<rootDir>/src/bin',
+    '<rootDir>/src/models',
+    '<rootDir>/dist/'
+  ],
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ]
 };
