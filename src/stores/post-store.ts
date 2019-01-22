367e8ede0802c2936a7301b27f8a6e5d7a74f277
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-01-22 15:36:18 -0200

    Transformed createPostStore and createUserStore into classes

--- src/stores/post-store.ts
+++ src/stores/post-store.ts
@@ -1,52 +1,59 @@
 import PostSchema, { Post } from '../models/post-model';
 
-export type PostStore = {
-  createPostStore: Function;
-};
+export default class PostStore {
+  logger: any;
 
-export function createPostStore(logger: any) {
-  return {
-    async find(offset = 0, limit = 20) {
-      logger.debug('Getting all the posts');
-      return await PostSchema.find({ parent: null })
-        .skip(offset)
-        .limit(limit)
-        .sort({
-          date: 'asc'
-        });
-    },
-    async findOneById(id: string) {
-      logger.debug(`Getting post with id ${id}`);
-      return await PostSchema.findById(id);
-    },
-    async create(post: Post) {
-      logger.debug(`Creating post with id ${post.id}`);
-      return await new PostSchema(post).save();
-    },
-    async update(id: String, post: Post) {
-      logger.debug(`Updating post with id ${id}`);
-      return await PostSchema.findByIdAndUpdate(id, post, { new: true });
-    },
-    async remove(id: String) {
-      logger.debug(`Removing post with id ${id}`);
-      return PostSchema.findByIdAndRemove(id);
-    },
-    async reply(reply: Post) {
-      logger.debug(`Replying post with id ${reply.parent}`);
+  constructor(logger: any) {
+    this.logger = logger;
+  }
+
+  async find(offset = 0, limit = 20) {
+    this.logger.debug('Getting all the posts');
+    return await PostSchema.find({ parent: null })
+      .skip(offset)
+      .limit(limit)
+      .sort({
+        date: 'asc'
+      });
+  }
+
+  async findOneById(id: string) {
+    this.logger.debug(`Getting post with id ${id}`);
+    return await PostSchema.findById(id);
+  }
+
+  async create(post: Post) {
+    this.logger.debug(`Creating post with id ${post.id}`);
+    return await new PostSchema(post).save();
+  }
+
+  async update(id: String, post: Post) {
+    this.logger.debug(`Updating post with id ${id}`);
+    return await PostSchema.findByIdAndUpdate(id, post, { new: true });
+  }
+
+  async remove(id: String) {
+    this.logger.debug(`Removing post with id ${id}`);
+    return PostSchema.findByIdAndRemove(id);
+  }
+
+  async reply(reply: Post) {
+    this.logger.debug(`Replying post with id ${reply.parent}`);
+    try {
+      this.logger.debug(
+        `Replying ${reply.parent} with \n ${JSON.stringify(reply, null, 2)} \n`
+      );
+      let parentPost = await PostSchema.findById(reply.parent);
+      const replyPost = new PostSchema(reply);
       try {
-        logger.debug(`Replying ${reply.parent} with \n ${JSON.stringify(reply, null, 2)} \n`);
-        let parentPost = await PostSchema.findById(reply.parent);
-        const replyPost = new PostSchema(reply);
-        try {
-          await replyPost.save();
-          parentPost.replies.push(replyPost);
-        } catch (err) {
-          return new Error("Couldn't create reply");
-        }
-        return parentPost.save();
-      } catch (error) {
-        throw error;
+        await replyPost.save();
+        parentPost.replies.push(replyPost);
+      } catch (err) {
+        return new Error("Couldn't create reply");
       }
+      return parentPost.save();
+    } catch (error) {
+      throw error;
     }
-  };
+  }
 }
