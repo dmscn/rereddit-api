cf671e44f418f8f467f8bdb6b51f57425a523dfd
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-01-23 15:10:36 -0200

    Changed service functions return to Promise<any>

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -33,18 +33,18 @@ export default class PostService {
   /**
    * @param {Number} [offset] Specify where the list start
    * @param {Number} [limit] Specify where the list ends
-   * @returns {Promise<Post>} List with all the Posts
+   * @returns {Promise<any>} List with all the Posts
    */
-  async find(options?: { offset: number; limit: number }): Promise<Post> {
+  async find(options?: { offset: number; limit: number }): Promise<any> {
     // @ts-ignore
     return await this.postStore.find(options);
   }
 
   /**
    * @param {String} id Id of the post
-   * @returns {Promise<Post>} The Post found
+   * @returns {Promise<any>} The Post found
    */
-  async findOneById(id: string): Promise<Post> {
+  async findOneById(id: string): Promise<any> {
     assertId(id);
     const post = await this.postStore.findOneById(id);
     return post || NotFound.makeAssert(`Post with id ${id} not found`);
@@ -52,9 +52,9 @@ export default class PostService {
 
   /**
    * @param {Post} post The Post that will be created
-   * @returns {Promise<Post>} Created Post
+   * @returns {Promise<any>} Created Post
    */
-  async create(post: Post): Promise<Post> {
+  async create(post: Post): Promise<any> {
     BadRequest.assert(post, 'Post inexistent');
     BadRequest.assert(post.title, 'No title');
     BadRequest.assert(post.content, 'No content');
@@ -65,18 +65,18 @@ export default class PostService {
 
   /**
    * @param {String} id Id of the Post that will be removed
-   * @returns {Promise<Post>} Empty Post
+   * @returns {Promise<any>} Empty Post
    */
-  async remove(id: string): Promise<Post> {
+  async remove(id: string): Promise<any> {
     assertId(id);
     return this.postStore.remove(id);
   }
 
   /**
    * @param {Post} post The Post that will be updated
-   * @returns {Promise<Post>} Updated Post
+   * @returns {Promise<any>} Updated Post
    */
-  async update(id: string, post: Post): Promise<Post> {
+  async update(id: string, post: Post): Promise<any> {
     assertId(id);
     BadRequest.assert(post, 'No post given');
     return await this.postStore.update(id, post);
@@ -84,9 +84,9 @@ export default class PostService {
 
   /**
    * @param {Post} post The reply Post containing the parent that will be replied
-   * @returns {Promise<Post>} The parent Post with it's replies
+   * @returns {Promise<any>} The parent Post with it's replies
    */
-  async reply(reply: Post): Promise<Post> {
+  async reply(reply: Post): Promise<any> {
     BadRequest.assert(reply, 'Reply Inexistent');
     BadRequest.assert(reply.parent, 'No post reference specified');
     BadRequest.assert(reply.content, 'No content');
