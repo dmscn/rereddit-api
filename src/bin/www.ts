4158c1ad2e89ad838a744dd370c6263d94809fd8
Leonardo Damasceno <ldamasceno.dev@gmail.com>
2019-02-03 19:58:49 -0200

    Fixed DATABASE env variable error

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -3,11 +3,13 @@ import { createServer } from '../lib/server';
 import { env } from '../lib/env';
 import { logger } from '../lib/logger';
 
-mongoose.connect(env.DATABASE, { useCreateIndex: true, useNewUrlParser: true });
-mongoose.connection.on('error', () => {
-  logger.error('Error connecting to MongoDB');
-  process.exit(1);
-});
+logger.info(`Database URI: ${env.DATABASE}`);
+mongoose
+  .connect(env.DATABASE, { useCreateIndex: true, useNewUrlParser: true })
+  .catch((err: Error) => {
+    logger.error(`Error connecting to MongoDB: ${err}`);
+    process.exit(1);
+  });
 
 createServer().then(
   app => {
